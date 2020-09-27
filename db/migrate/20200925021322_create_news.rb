522c7762ccfbd364ac7bd57d59aab9cb2f453766
SWillames <sergiowoc@gmail.com>
2020-09-27 14:17:39 -0300

    Correcting some errors and adding the ffaker gem in the user factory.

--- db/migrate/20200925021322_create_news.rb
+++ db/migrate/20200925021322_create_news.rb
@@ -3,10 +3,14 @@ class CreateNews < ActiveRecord::Migration[6.0]
     create_table :news do |t|
       t.string :title
       t.string :slug
+      t.text :tldr
       t.text :introduction
       t.text :body
       t.date :date
       t.string :url_image
+      t.string :tag
+      t.string :tag1
+      t.string :tag2
       t.references :user, null: false, foreign_key: true
 
       t.timestamps
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,11 @@
+require 'ffaker'
+
 FactoryBot.define do
   factory :user do
-    
+    first_name {FFaker::Name.first_name}
+    last_name {FFaker::Name.last_name}
+    user_name {FFaker::InternetSE.user_name("#{first_name} - #{last_name}") }
+    email {FFaker::Internet.safe_email}
+    password {FFaker::Internet.password}
   end
 end
